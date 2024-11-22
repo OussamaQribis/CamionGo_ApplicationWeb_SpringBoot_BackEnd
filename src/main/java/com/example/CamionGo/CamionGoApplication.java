c0d6aec69797d4269fa81e6102c7a05db7d6e0e1
OussamaQribis <ossamaqribis@gmail.com>
2024-11-22 15:23:20 +0100

    first commit

---
+++ src/main/java/com/example/CamionGo/CamionGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CamionGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamionGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamionGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CamionGo/CamionGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CamionGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamionGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
